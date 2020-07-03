a0fc8b00754a4e27719bd23375050854bde8534b
Joseph Haberberger <joe.haberberger@qflow.com>
2020-07-03 16:03:00 -0600

    adding heroes list and a bit of styling

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 
 @Component({
   selector: 'app-heroes', //top-layer <selector />
@@ -10,6 +11,8 @@ export class HeroesComponent implements OnInit {
   //iterpolation variable
   // iterpolation = 'This called interpolation';
   hero: Hero = { id: 1, name: 'Windstorm' };
+
+  heroes: Hero[] = HEROES;
 
   constructor() {}
 
