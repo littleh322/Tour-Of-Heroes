283588c49602686a51e039385efcc52653173c39
Joseph Haberberger <joe.haberberger@qflow.com>
2020-07-11 13:27:35 -0600

    issue #12 - fix details view

--- src/app/services/hero-service/hero.service.ts
+++ src/app/services/hero-service/hero.service.ts
@@ -17,6 +17,6 @@ export class HeroService {
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES[id]);
+    return of(HEROES.find((hero) => (hero.id = id)));
   }
 }
